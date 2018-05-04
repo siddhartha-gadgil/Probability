0d2314f37495c61d445f221ee2d1d336de2eb82e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-05-04 13:07:42 +0530

    very crude Html for taking stock

--- sitebuilder/src/TexToHtml.scala
+++ sitebuilder/src/TexToHtml.scala
@@ -64,9 +64,115 @@ object TeXToHtml {
 
   val Array(header, text) = fullText.split("\\\\begin\\{document\\}")
 
-  val begReg = """\\begin\{([^\}]+)\}""".r
+  val begReg = """\\begin\{([^\}]+)\}\s""".r
+
+  val endReg = """\\end\{([^\}]+)\}\s""".r
 
   val converter = new TeXToHtml(header, text)
+
+  val dolReg = "[^\\$]\\$[^\\$]".r
+
+  val doldolReg = "\\$\\$".r
+
+  val begItReg = """\\begin\{itemize\}""".r
+
+  val endItReg = """\\end\{itemize\}""".r
+
+  val begEnReg = """\\begin\{enumerate\}""".r
+
+  val endEnReg = """\\begin\{enumerate\}""".r
+
+  val blankLineReg = "\n([ \t]*\n)".r
+
+  def maxOpt[B: Ordering](v: Vector[B]): Option[B] = if (v.isEmpty) None else Some(v.max)
+
+
+  val thmEnvs = Set(
+    "example",
+    "question",
+    "definition",
+    "remark",
+    "exercise",
+    "lemma",
+    "problem",
+    "proposition",
+    "theorem"
+  )
+
+  val mathEnvs = Set(
+    "align*",
+    "equation*",
+    "aligned",
+    "array",
+    "equation",
+    "tabular"
+  )
+
+
+
+  def indices(r: Regex, txt: String): Vector[Int] = r.findAllMatchIn(txt).toVector.map(_.start)
+
+  def dollarIndices(txt: String) : Vector[Int] = indices(dolReg, txt).map(_ + 1)
+
+  def displayIndices(txt: String) : Vector[Int] = indices(doldolReg, txt)
+
+  def itemizeBegs(txt: String): Vector[Int] = indices(begItReg, txt)
+
+
+  def itemizeEnds(txt: String): Vector[Int] = indices(endItReg, txt)
+
+  def enumerateBegs(txt: String): Vector[Int] = indices(begEnReg, txt)
+
+  def enumerateEnds(txt: String): Vector[Int] = indices(begEnReg, txt)
+
+  def items(txt: String): Vector[Int] = indices("""\\item""".r, txt)
+
+  def firstItem(j: Int, txt: String) : Boolean = {
+    val lastBeg = (enumerateBegs(txt) ++ itemizeBegs(txt)).filter(_ < j).max
+    maxOpt(items(txt).filter(_ < j)).map{
+      _ < lastBeg
+    }.getOrElse(true)
+
+  }
+
+  def inMath(j: Int, txt: String) : Boolean = dollarIndices(txt).filter(_ < j).size % 2 == 1
+
+  def inDisplayMath(j: Int, txt: String) : Boolean = displayIndices(txt).filter(_ < j).size % 2 == 1
+
+
+  def replaceItems(txt: String): String = {
+    val itemLess =
+      """\\item""".r.replaceAllIn(txt,
+        (m) => if (firstItem(m.start, txt)) "<li>" else "</li>\n<li>"
+      )
+    itemLess.replace("""\begin{itemize}""", "<ul>")
+      .replace("""\end{itemize}""", "</ul>")
+      .replace("""\begin{enumerate}""", "<ol>")
+      .replace("""\end{enumerate}""", "</ol>")
+  }
+
+  def replaceBegins(txt: String) =
+    begReg.replaceAllIn(txt,
+      (m) =>
+//        if (inMath(m.start, txt) || inDisplayMath(m.start, txt)) m.group(0)
+//        else
+        if (thmEnvs.contains(m.group(1))) s"""<div class="${m.group(1)}">"""
+        else if (mathEnvs.contains(m.group(1))) Regex.quoteReplacement("$$"+m.group(0))
+        else s"""<div class="${m.group(1)}">"""
+    )
+
+  def replaceEnds(txt: String) =
+    endReg.replaceAllIn(txt,
+      (m) =>
+//        if (inMath(m.start, txt) || inDisplayMath(m.start, txt)) {println(s"${m.group(0)} in math environment"); m.group(0)}
+//        else
+        if (mathEnvs.contains(m.group(1))) Regex.quoteReplacement(m.group(0)+"$$")
+        else "</div>"
+    )
+
+  def replaceBlanks(txt: String) = blankLineReg.replaceAllIn(txt, "</p>\n<p>")
+
+
 }
 
 class TeXToHtml(header: String, text: String) {
@@ -101,14 +207,17 @@ class TeXToHtml(header: String, text: String) {
 
   lazy val defReplaced = recDefReplace(text)
 
+    lazy val allReplaced = replaceBlanks(replaceEnds(replaceBegins(replaceItems(defReplaced))))
+
   lazy val newFile = header + """\begin{document}""" + defReplaced
 
   def replace = write.over(wd / "repl.tex", newFile)
+
+  lazy val crudeHtml = top + allReplaced + foot
+
+  def html = write.over(pwd / "docs" / "crude" / "index.html", crudeHtml)
 }
 
-object NewTex extends App {
-  TeXToHtml.converter.replace
-}
 /*
 The cases for begin{_} :
 Vector(
