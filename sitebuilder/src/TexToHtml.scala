61b3bc83c81a99c0fdf48a505677c2017029ef48
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-05-05 22:27:26 +0530

    draft with navigation, sections; bugs remain

--- sitebuilder/src/TexToHtml.scala
+++ sitebuilder/src/TexToHtml.scala
@@ -9,9 +9,21 @@ object TeXToHtml {
     """
 <html>
 <head>
+<title> Probability Models and Stastics</title>
+<link rel="icon" href="../IIScLogo.jpg">
 
 <!-- Latest compiled and minified CSS  for Bootstrap -->
 <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+<style type="text/css">
+   body { padding-top: 60px; }
+   .section {padding-top: 60px;}
+   #arxiv {
+     border-style: solid;
+     border-width: 1px;
+   }
+</style>
+
 
   <!-- mathjax config similar to math.stackexchange -->
   <script type="text/javascript" async
@@ -34,8 +46,66 @@ object TeXToHtml {
   });
   </script>
 </head>
-<body>
+<body>"""
+
+  def nav(part1: String, part2: String, part3: String) =
+    s"""
+    |<nav class="navbar navbar-default navbar-fixed-top">
+    |    <div class="container-fluid">
+    |      <!-- Brand and toggle get grouped for better mobile display -->
+    |      <div class="navbar-header">
+    |        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    |          <span class="sr-only">Toggle navigation</span>
+    |          <span class="icon-bar"></span>
+    |          <span class="icon-bar"></span>
+    |          <span class="icon-bar"></span>
+    |        </button>
+    |        <span class="navbar-brand">Probability and Statistics (notes by Manjunath Krishnapur)</span>
+    |      </div>
+    |
+    |      <!-- Collect the nav links, forms, and other content for toggling -->
+    |      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    |
+    |        <ul class="nav navbar-nav navbar-right">
+    |
+    |          <li class="dropdown">
+    |            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Part 1 <span class="caret"></span></a>
+    |            <ul class="dropdown-menu">
+    |             $part1
+    |            </ul>
+    |          </li>
+    |
+    |          <li class="dropdown">
+    |            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Part 2 <span class="caret"></span></a>
+    |            <ul class="dropdown-menu">
+    |             $part2
+    |            </ul>
+    |          </li>
+    |
+    |          <li class="dropdown">
+    |            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Part 3 <span class="caret"></span></a>
+    |            <ul class="dropdown-menu">
+    |             $part3
+    |            </ul>
+    |          </li>
+    |        </ul>
+    |      </div><!-- /.navbar-collapse -->
+    |    </div><!-- /.container-fluid -->
+    |  </nav>
+    |
+  """.stripMargin
+
+  val banner =
+    """
 <div class="container">
+<div class="bg-primary">
+     <div class="banner">
+
+     <center><h2 style="margin-top: 2px;"> Probability and Statistics </h2></center>
+     <center><h4 style="margin-bottom: 2px;"> Notes by Manjunath Krishnapur </h4></center>
+   </div>
+ </div>
+ <section>
 <p>&nbsp;</p>
 
 """
@@ -43,8 +113,10 @@ object TeXToHtml {
   val foot =
     """
 </div>
+|<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+ |<script type="text/javascript" src='../js/bootstrap.min.js'></script>
+ |
   </body>
-</body>
 </html>
 """
 
@@ -53,7 +125,8 @@ object TeXToHtml {
 
   val newCommReg: Regex = "(\\\\newcommand\\{)(\\\\[a-zA-Z0-9]+)(\\}[^\n%]+)".r
 
-  val renewCommReg: Regex = "(\\\\renewcommand\\{)(\\\\[a-zA-Z0-9]+)(\\}[^\n%]+)".r
+  val renewCommReg: Regex =
+    "(\\\\renewcommand\\{)(\\\\[a-zA-Z0-9]+)(\\}[^\n%]+)".r
 
   def trimLine(l: String): String =
     if (l.startsWith("%")) ""
@@ -63,7 +136,8 @@ object TeXToHtml {
         .map((m) => m.before.toString + m.group(0).head)
         .getOrElse(l)
 
-  def readTrim(f: Path): String = read.lines(f).map(trimLine).mkString("", "\n", "\n")
+  def readTrim(f: Path): String =
+    read.lines(f).map(trimLine).mkString("", "\n", "\n")
 
   val wd: Path = pwd / 'sitebuilder / "resources"
 
@@ -73,18 +147,24 @@ object TeXToHtml {
 
   val fullText: String = texFile.replace("\\input{mypreamble_2017}", preamble)
 
-  val Array(header: String, textPadded: String) = fullText.split("\\\\begin\\{document\\}")
+  val Array(header: String, textPadded: String) =
+    fullText.split("\\\\begin\\{document\\}")
 
   val text: String = textPadded
     .split("""\\maketitle""")
     .last
-    .split("""\\begin\{thebibliography\}""")
+    .split("""\\end\{document\}""")
     .head
+    .replace("<", " < ")
+    .replace(">", " > ")
+    .replace("~", " ")
+    .replace("\\newpage", "")
+    .replace("\\vspace{4mm}", "")
 
   val begReg: Regex = """\\begin\{([^\}\{]+|[^\{\}]*\{[^\{\}]*\}[^\{\}]*)\}""".r
 
   val fullBegReg: Regex =
-    """\\begin\{([a-zA-Z0-9\*]*)\}(\[[^\[\]]+\])?(\\label\{[a-zA-Z0-9:+-_]+\})?""".r
+    """\\begin\{([a-zA-Z0-9\*]*)\}([\s]*\[[^\[\]]+\])?([\s]*\\label\{[a-zA-Z0-9:+-_]+\})?""".r
 
   val endReg = """\\end\{([^\}\{]+|[^\{\}]*\{[^\{\}]*\}[^\{\}]*)\}""".r
 
@@ -188,22 +268,6 @@ object TeXToHtml {
         else s"""<div class="${m.group(1)}">"""
     )
 
-//  def rplBegins(txt: String, head: String = ""): String = {
-//    begReg
-//      .findFirstMatchIn(txt)
-//      .map { (m) =>
-//        val newString =
-//          if (thmEnvs.keySet.contains(m.group(1))) s"""<div class="${m
-//            .group(1)}"> <strong>${thmEnvs(m.group(1))}</strong> """
-//          else if (mathEnvs.contains(m.group(1)))
-//            Regex.quoteReplacement("$$" + m.group(0))
-//          else if (divEnvs.contains(m.group(1))) s"""<div class="${m.group(1)}">"""
-//            else Regex.quoteReplacement(m.group(0))
-//        rplBegins(m.after.toString, head + m.before.toString + newString)
-//      }
-//      .getOrElse(head + txt)
-//  }.replace("""\\begin""", """\begin""").replace("""\$""", "$")
-
   def recRplBegins(
       txt: String,
       head: String = "",
@@ -214,7 +278,7 @@ object TeXToHtml {
       .map { (m) =>
         val title = Option(m.group(2))
         val labelOpt =
-          Option(m.group(3)).map(_.drop("""\label{""".size).dropRight(1))
+          Option(m.group(3)).map(_.trim.drop("""\label{""".size).dropRight(1))
         val newCounter =
           if (thmEnvs.keySet.contains(m.group(1))) thmCounter + 1
           else thmCounter
@@ -223,12 +287,14 @@ object TeXToHtml {
             labelOpt.map((l) => labels + (l -> newCounter)).getOrElse(labels)
           else labels
         val newString =
-          if (thmEnvs.keySet.contains(m.group(1))) s"""<div class="${m
-            .group(1)}"> <strong>${thmEnvs(m.group(1))} $newCounter ${title
-            .map((s) => "(" + s.drop(1).dropRight(1) + ")")
-            .getOrElse("")}</strong>"""
+          if (thmEnvs.keySet.contains(m.group(1)))
+            s"""<div id="theorem-${newCounter}"><div class="panel panel-info ${m
+              .group(1)}"> <div class="panel-heading">${thmEnvs(m.group(1))} $newCounter ${title
+              .map((s) => "(" + s.drop(1).dropRight(1) + ")")
+              .getOrElse("")}</div><div class="panel-body">"""
           else if (mathEnvs.contains(m.group(1)))
-            Regex.quoteReplacement("$$" + m.group(0))
+            Regex.quoteReplacement(
+              "$$" + m.group(0).replace("""\label""", """\tag"""))
           else if (divEnvs.contains(m.group(1)))
             s"""<div class="${m.group(1)}">"""
           else Regex.quoteReplacement(m.group(0))
@@ -240,10 +306,11 @@ object TeXToHtml {
       .getOrElse((head + txt, thmCounter, labels))
   }
 
-  def rplBegins(txt: String): String =
-    recRplBegins(txt)._1
-      .replace("""\\begin""", """\begin""")
-      .replace("""\$""", "$")
+  def rplBegins(txt: String): (String, Map[String, Int]) =
+    (recRplBegins(txt)._1
+       .replace("""\\begin""", """\begin""")
+       .replace("""\$""", "$"),
+     recRplBegins(txt)._3)
 
   def replaceEnds(txt: String) =
     endReg.replaceAllIn(
@@ -253,7 +320,8 @@ object TeXToHtml {
 //        else
         if (mathEnvs.contains(m.group(1)))
           Regex.quoteReplacement(m.group(0) + "$$")
-        else if (thmEnvs.keySet.union(divEnvs).contains(m.group(1))) "</div>"
+        else if (thmEnvs.keySet.contains(m.group(1))) "</div></div></div>"
+        else if (divEnvs.contains(m.group(1))) "</div>"
         else Regex.quoteReplacement(m.group(0))
     )
 
@@ -279,6 +347,41 @@ object TeXToHtml {
         txt,
         (m) => s"""<strong>${m.group(2)} .</strong><p class="text-justify">""")
 
+  def recReplaceSubSection(txt: String,
+                           head: String,
+                           counter: Int,
+                           section: Int): String =
+    """(\\subsection\{)([^\}\{]+|[^\{\}]*\{[^\{\}]*\}[^\{\}]*)\}""".r
+      .findFirstMatchIn(txt)
+      .map { (m) =>
+        val newHead =
+          s"""<strong>${section}.${counter + 1} ${m.group(2)}.</strong><p class="text-justify">"""
+        recReplaceSubSection(m.after.toString, newHead, counter + 1, section)
+      }
+      .getOrElse(head + txt)
+
+  def recReplaceSection(
+      txt: String,
+      head: String = "",
+      counter: Int = 0,
+      secs: Map[Int, String] = Map()): (String, Map[Int, String]) =
+    """(\\section\{)([^\}\{]+|[^\{\}]*\{[^\{\}]*\}[^\{\}]*)\}""".r
+      .findFirstMatchIn(txt)
+      .map { (m) =>
+        val title = m.group(2).toString
+        val prev = recReplaceSubSection(m.before.toString, "", 0, counter)
+        val newHead =
+          s"""$head$prev</section>
+             <section><h2 id="section-${counter + 1}">${counter + 1}. ${title}.</h2><p class="text-justify">""".stripMargin
+        recReplaceSection(
+          m.after.toString,
+          newHead,
+          counter + 1,
+          secs + ((counter + 1) -> title)
+        )
+      }
+      .getOrElse(head + txt -> secs)
+
   def replacePara(txt: String) =
     """(\\para\{)([^\}\{]+|[^\{\}]*\{[^\{\}]*\}[^\{\}]*)\}""".r
       .replaceAllIn(
@@ -298,8 +401,20 @@ object TeXToHtml {
   def replaceBf(txt: String) = {
     val step = bfReg1.replaceAllIn(
       txt,
-      (m) => Regex.quoteReplacement(s"<strong>${m.group(2)}</strong>"))
-    bfReg2.replaceAllIn(step, (m) => s"<strong>${m.group(2)}</strong>")
+      (m) =>
+        if (dolReg
+              .findAllIn(txt)
+              .size % 2 == 0 && dolReg.findAllIn(txt).size % 2 == 0)
+          Regex.quoteReplacement(s"<strong>${m.group(2)}</strong>")
+        else m.group(2))
+    bfReg2.replaceAllIn(
+      step,
+      (m) =>
+        if (dolReg
+              .findAllIn(txt)
+              .size % 2 == 0 && dolReg.findAllIn(txt).size % 2 == 0)
+          Regex.quoteReplacement(s"<strong>${m.group(2)}</strong>")
+        else m.group(2))
   }
 
   val emReg = "(\\{\\\\em )([^\\}]+)\\}".r
@@ -347,15 +462,38 @@ class TeXToHtml(header: String, text: String) {
         replaceBf(
           replacePara(replaceParag(replaceBlanks(replaceItems(defReplaced)))))))
 
-  lazy val allReplaced = replaceSec(rplBegins(baseReplaced))
+  lazy val (begReplaced, labels) = rplBegins(baseReplaced)
+
+  lazy val refReplaced =
+    """\\ref\{([a-zA-Z0-9:+-_]+)\}""".r.replaceAllIn(
+      begReplaced,
+      (m) =>
+        labels
+          .get(m.group(1))
+          .map((n) => s"""<a href="#theorem-$n">$n</a>""")
+          .getOrElse(m.group(0)))
+
+  lazy val (allReplaced, sections) = recReplaceSection(refReplaced)
+
+  lazy val sortedSections: Vector[(Int, String)] = sections.toVector.sortBy(_._1)
+
+  def sectionList(v: Vector[(Int, String)]): String =
+    v.map {
+        case (n, title) =>
+          s"""<li><a href="#section-${n}">${n}. $title</a> </li>"""
+      }
+      .mkString("\n")
 
   lazy val newFile = header + """\begin{document}""" + defReplaced
 
   def replace() = write.over(wd / "repl.tex", newFile)
 
-  lazy val crudeHtml = top + allReplaced + foot
+  lazy val draftHtml = top + nav(
+    sectionList(sortedSections.take(15)),
+    sectionList(sortedSections.drop(15).take(15)),
+    sectionList(sortedSections.drop(30))) + banner + allReplaced + foot
 
-  def html() = write.over(pwd / "docs" / "crude" / "index.html", crudeHtml)
+  def html() = write.over(pwd / "docs" / "draft" / "index.html", draftHtml)
 }
 
 object CrudeBuild extends App {
