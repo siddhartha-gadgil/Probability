8036e4251df6879697fc2e56caf6a16ffbe5905f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-05-06 07:06:57 +0530

    mobile friendly

--- sitebuilder/src/TexToHtml.scala
+++ sitebuilder/src/TexToHtml.scala
@@ -9,6 +9,10 @@ object TeXToHtml {
     """
 <html>
 <head>
+<meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title> Probability Models and Stastics</title>
 <link rel="icon" href="../IIScLogo.jpg">
 
